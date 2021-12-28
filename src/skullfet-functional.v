3eaa6468cb870232c107fa4e43e943eb633702b2
Uri Shaked <uri@urishaked.com>
2021-12-28 15:05:54 +0200

    feat: add power pins to functional model

--- src/skullfet-functional.v
+++ src/skullfet-functional.v
@@ -1,5 +1,20 @@
 `default_nettype none
 
-module skullfet_inverter(input wire A, output wire Y);
-    assign Y = !A;
+module skullfet_inverter (
+`ifdef USE_POWER_PINS
+    input VGND,
+    input VPWR,
+`endif  // USE_POWER_PINS
+    input wire A,
+    output wire Y
+);
+`ifdef USE_POWER_PINS
+  wire power_good = VPWR == 1 && VGND == 0;
+`else
+  wire power_good = 1'b1;
+`endif  // USE_POWER_PINS
+
+  wire buf_not_A;
+  buf buf0 (buf_not_A, !A);
+  assign Y = power_good ? !buf_not_A : 1'bx;
 endmodule
--- src/skullfet.v
+++ src/skullfet.v
@@ -1,5 +1,10 @@
 (* blackbox *)
-module skullfet_inverter(A, Y);
-    input A;
-    output Y;
+module skullfet_inverter (
+`ifdef USE_POWER_PINS
+    input  VGND,
+    input  VPWR,
+`endif  // USE_POWER_PINS
+    input  A,
+    output Y
+);
 endmodule
